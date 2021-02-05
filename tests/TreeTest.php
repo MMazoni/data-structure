6577998577a5cfffa3288d4f6ffd7cb4395028e4
Matheus Mazoni <mmazoni.andrade@gmail.com>
2021-02-05 07:58:38 -0300

    more test in tree

--- tests/TreeTest.php
+++ tests/TreeTest.php
@@ -18,7 +18,35 @@ final class TreeTest extends TestCase
         $this->assertEquals($cfo, $ceo->addChildren($cfo));
         $this->assertEquals($cmo, $ceo->addChildren($cmo));
         $this->assertEquals($coo, $ceo->addChildren($coo));
+        $seniorArchitect = new TreeNode("Senior Architect");
+        $softwareEngineer = new TreeNode("Software Engineer");
+        $userInterfaceDesigner = new TreeNode("User Interface Designer");
+        $qualityAssuranceEngineer = new TreeNode("Quality Assurance Engineer");
 
+        $this->assertEquals($seniorArchitect, $cto->addChildren($seniorArchitect));
+        $this->assertEquals($softwareEngineer, $seniorArchitect->addChildren($softwareEngineer));
+        $this->assertEquals($qualityAssuranceEngineer, $cto->addChildren($qualityAssuranceEngineer));
+        $this->assertEquals($userInterfaceDesigner, $cto->addChildren($userInterfaceDesigner));
+
+        return $tree;
+    }
+
+    /**
+     * @depends testTreeCanAddChildren
+     */
+    public function testTreeCanTraverse(Tree $tree): Tree
+    {
+        $expected = 'CEO
+-CTO
+--Senior Architect
+---Software Engineer
+--Quality Assurance Engineer
+--User Interface Designer
+-CFO
+-CMO
+-COO'.PHP_EOL;
+        $this->expectOutputString($expected);
+        $tree->traverse($tree->root);
         return $tree;
     }
 }

37251abcd56c9026442190dd3c4a1db2b2542af0
Matheus Mazoni <mmazoni.andrade@gmail.com>
2021-02-11 23:01:25 -0300

    implementing binary search tree

---
+++ src/Nonlinear/BinarySearchTree.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace MMazoni\DataStructure\Nonlinear;
+
+class BinarySearchTree
+{
+    public ?Node $root = null;
+
+    public function __construct(int $data) {
+        $this->root = new Node($data);
+    }
+
+    public function isEmpty(): bool
+    {
+        return $this->root === null;
+    }
+
+    public function insert(int $data): Node
+    {
+        if ($this->isEmpty()) {
+            $node = new Node($data);
+            $this->root = $node;
+            return $node;
+        }
+
+        $node = $this->root;
+        while ($node) {
+            if ($data > $node->data) {
+                if ($node->right)
+                    $node = $node->right;
+                else {
+                    $node->right = new Node($data);
+                    $node = $node->right;
+                    break;
+                }
+            } elseif ($data < $node->data) {
+                if ($node->left)
+                    $node = $node->left;
+                else {
+                    $node->left = new Node($data);
+                    $node = $node->left;
+                    break;
+                }
+            } else {
+                break;
+            }
+        }
+        return $node;
+    }
+
+    public function traverse(Node $node): void
+    {
+        if ($node) {
+            if ($node->left)
+                $this->traverse($node->left);
+            echo $node->data . PHP_EOL;
+            if ($node->right)
+                $this->traverse($node->right);
+        }
+    }
+}
---
+++ src/Nonlinear/Node.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace MMazoni\DataStructure\Nonlinear;
+
+class Node
+{
+    public ?Node $left;
+    public ?Node $right;
+
+    public function __construct(public mixed $data = null) {
+        $this->data = $data;
+        $this->left = null;
+        $this->right = null;
+    }
+
+    public function min() : Node
+    {
+        $node = $this;
+        while ($node->left) {
+            $node = $node->left;
+        }
+        return $node;
+    }
+
+    public function max() : Node
+    {
+        $node = $this;
+        while ($node->right) {
+            $node = $node->right;
+        }
+        return $node;
+    }
+
+    public function sucessor(): Node
+    {
+        $node = $this;
+        if ($node->right)
+            return $node->right->min();
+
+        return null;
+    }
+
+    public function predecessor(): Node
+    {
+        $node = $this;
+        if ($node->left)
+            return $node->left->max();
+
+        return null;
+    }
+}

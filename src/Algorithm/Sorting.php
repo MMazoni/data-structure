5536aba69feb8d2501c89dfd3f2f68b8b1691334
Matheus Mazoni <mmazoni.andrade@gmail.com>
2021-02-19 22:24:28 -0300

    Bubble sort implementation

---
+++ src/Algorithm/Sorting.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace MMazoni\DataStructure\Algorithm;
+
+class Sorting
+{
+    public static function bubbleSort(array $array): array
+    {
+        $lenght = count($array);
+
+        for ($i = 0; $i < $lenght; $i++) {
+            for ($j = 0; $j < $lenght - 1; $j++) {
+                if ($array[$j] > $array[$j + 1]) {
+                    $temporary = $array[$j + 1];
+                    $array[$j + 1] = $array[$j];
+                    $array[$j] = $temporary;
+                }
+            }
+        }
+        return $array;
+    }
+}
---
+++ tests/SortingTest.php
@@ -0,0 +1,16 @@
+<?php
+
+use MMazoni\DataStructure\Algorithm\Sorting;
+use PHPUnit\Framework\TestCase;
+
+final class SortingTest extends TestCase
+{
+    public function testCanSortingArray(): void
+    {
+        $expected = '10,20,33,45,52,67,88,92,93,97';
+        $this->expectOutputString($expected);
+        $array = [20, 45, 93, 67, 10, 97, 52, 88, 33, 92];
+        $sortedArray = Sorting::bubbleSort($array);
+        echo implode(",", $sortedArray);
+    }
+}

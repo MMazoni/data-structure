86a2e4e281a8a5b3a9ac79699490873d08bec138
Matheus Mazoni <mmazoni.andrade@gmail.com>
2021-02-21 23:49:53 -0300

    Improved bubble sort

--- src/Algorithm/Sorting.php
+++ src/Algorithm/Sorting.php
@@ -9,13 +9,17 @@ class Sorting
         $lenght = count($array);
 
         for ($i = 0; $i < $lenght; $i++) {
-            for ($j = 0; $j < $lenght - 1; $j++) {
+            $swapped = false;
+            for ($j = 0; $j < $lenght - $i - 1; $j++) {
                 if ($array[$j] > $array[$j + 1]) {
                     $temporary = $array[$j + 1];
                     $array[$j + 1] = $array[$j];
                     $array[$j] = $temporary;
+                    $swapped = true;
                 }
             }
+            if (!$swapped)
+                break;
         }
         return $array;
     }
